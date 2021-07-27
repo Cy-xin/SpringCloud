ce89a19855c34c681218f5cfe1fd10642a8e945e
XinTianWeng <1257425345@qq.com>
2021-07-27 21:25:38 +0800

    create eureka service and eureka client

--- cloud-consumer-order80/src/main/java/com/spring/springcloud/OrderMain.java
+++ cloud-consumer-order80/src/main/java/com/spring/springcloud/OrderMain.java
@@ -2,11 +2,13 @@ package com.spring.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author xintianweng
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain {
 
     public static void main(String[] args) {
--- cloud-eureka-server7001/src/main/java/com/spring/springcloud/EurekaMain.java
+++ cloud-eureka-server7001/src/main/java/com/spring/springcloud/EurekaMain.java
@@ -2,17 +2,17 @@ package com.spring.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author xintianweng
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain {
+@EnableEurekaServer
+public class EurekaMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain.class, args);
+        SpringApplication.run(EurekaMain.class, args);
     }
 
 }

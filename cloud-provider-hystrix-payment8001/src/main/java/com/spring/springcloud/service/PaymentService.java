65e23d64479befa3dcbc06b21db84ee4fa5e63b5
XinTianWeng <1257425345@qq.com>
2021-08-03 23:07:16 +0800

    hystirx falllback服务降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/spring/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/spring/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.spring.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -19,13 +21,20 @@ public class PaymentService {
         return "线程池:  " + Thread.currentThread().getName() + "  paymentInfo_OK,id:  " + id + "\t" + "O(∩_∩)O哈哈~";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfo_TimeOut(Integer id) {
         try {
-            TimeUnit.MILLISECONDS.sleep(3000);
+            TimeUnit.MILLISECONDS.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池:  " + Thread.currentThread().getName() + " id:  " + id + "\t" + "O(∩_∩)O哈哈~" + "  耗时(秒): ";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "线程池:  " + Thread.currentThread().getName() + "paymentInfo_TimeOutHandler， id:  " + id + "\t" + "o(╥﹏╥)o";
     }
 
 }
